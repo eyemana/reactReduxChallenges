1199bf57cfcaa2fb1a1170280d3c9f7c556c67d8
jalopy67 <i_evans@hotmail.com>
2020-04-29 13:31:54 -0700

    finished the course.  Implemented all features.  STill need to do the challenge assignments.

--- babel.config.js
+++ babel.config.js
@@ -2,7 +2,10 @@ module.exports = function (api) {
   api.cache(true);
 
   const presets = ["@babel/preset-env", "@babel/preset-react"];
-  const plugins = ["@babel/transform-runtime"];
+  const plugins = [
+    "@babel/transform-runtime",
+    "@babel/plugin-proposal-class-properties",
+  ];
 
   return {
     presets,
--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,15 +1,7 @@
-import { createStore, applyMiddleware, compose } from "redux";
-import rootReducer from "./reducers";
-import reduxImmutableStateInvariant from "redux-immutable-state-invariant";
-import thunk from "redux-thunk";
-
-export default function configureStore(initialState) {
-  const composeEnhancers =
-    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose; // add support for Redux dev tools
-
-  return createStore(
-    rootReducer,
-    initialState,
-    composeEnhancers(applyMiddleware(thunk, reduxImmutableStateInvariant()))
-  );
+if (process.env.NODE_ENV === "production") {
+  module.exports = require("./configureStore.prod");
+} else {
+  module.exports = require("./configureStore.dev");
 }
+
+// dynamically loading the appropriate file at build time using CommonJS syntax for import/export.
